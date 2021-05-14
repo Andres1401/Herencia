81b583e1a4b50746886f81bbc16fea324c087f26
mandi <mandi@192.168.1.69>
2021-05-14 12:07:29 -0700

    Version 1

---
+++ src/main/java/AlumnoPrimaria.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mandi
+ */
+public class AlumnoPrimaria extends Alumnos {
+    private String grado;
+
+    public AlumnoPrimaria(char sexo, int edad, String nombre) {
+        super(sexo, edad, nombre);
+    }
+    
+    
+}
